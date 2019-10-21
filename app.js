d2e0f5f3bb0c2dfa5718797afbc2bf38886ed3a0
ilsalareb1 <50841067+ilsalareb1@users.noreply.github.com>
2019-10-20 22:57:32 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-///one class to create the object person, album, movie ***Model
-//take arguments 
+//one class to create the object person, album, movie ***Model
 class Movie{
     constructor(title, director, leadActor1, leadActor2, yearReleased){
         this.tile = title;
@@ -11,39 +10,15 @@ class Movie{
      calculateAge = ()=> new Date().getFullYear() - this.yearReleased;
 }
 
-const Movies =[];
+//UI class with methods
+class UI {
 
-/* const JurrasicPark = new Movie('Jurassic Park', 'Steven Speilberg', 'Jeff Goldblum', 'Steven Neil',1993); */
-
-
-//push into array
-function addMovie(title, director, leadActor1, leadActor2, yearReleased, moviePoster) {
-    Movie.push(new Movie (title, director, leadActor1, leadActor2, yearReleased, moviePoster) );
-}
-
-
-//display objects on screen 
-const form = document.querySelector('#movies-form');
-const userTitle = document.querySelector('#title');
-const userDirector = document.querySelector('#director');
-const userleadActor1 = document.querySelector('#leadActor1');
-const userleadActor2 = document.querySelector('#leadActor2');
-const userYearReleased = document.querySelector('#yearReleased');
-const userMoviePoster = document.querySelector('#image');
-
-
-function eventListeners(){
-    form.addEventListener('submit', displayMovie);
-    display.addEventListener('click', removeMovie);
-
-}
-
-
-//creates/insert the HTML for the UI display 
-function displayMovie(e){
-    if (userTitle.value == false || userDirector.value == false || userleadActor1.value == false || userleadActor2.value == false || userYearReleased.value == false){
-        alert('Please input all values');
-    }else {
+    displayMovie(movie){
+        if (userTitle.value == false || userDirector.value == false || userleadActor1.value == false || userleadActor2.value == false || userYearReleased.value == false){
+            alert('Please input all values');
+        }else {
+        const display= document.querySelector('.display');
+        
         let html='<div class="display-movie"><div class="display-title">%title%</div><div class="display-director">%director%</div><div class= "display-leadActor-1"> %leadActor-1%</div><div class="display-leadActor2"> %leadActor-2% </label></div><div class="display-yearReleased">%yearReleased%</div><div class="display-image"><img src="%url%"></div><div class="remove-movie"><p class="remove-movie">Remove Movie &#10006; </p></div></div>';
 
         let newHtml = html.replace('%title%', userTitle.value);
@@ -54,23 +29,64 @@ function displayMovie(e){
         newHtml = html.replace('%url%', userMoviePoster.value);
         display.insertAdjacentHTML('beforeend', newHtml);
         e.preventDefault();
-
-
+        }
     }
+
+    //clear methods 
+    clearValues(){
+        document.getElementById("movie-form").reset();
+    }
+
+    //remove object
+    removeMovie(e){
+        if (e.parentElement.classList.contains('display-button')){
+            e.parentElement.parentElement.remove();
+        }
+    }
+}
+
+
+
+
+//event listener controller 
+document.getElementById('form').addEventListener('submit', function(e){
+    //values
+    const form = document.querySelector('#movies-form');
+    const userTitle = document.querySelector('#title');
+    const userDirector = document.querySelector('#director');
+    const userleadActor1 = document.querySelector('#leadActor1');
+    const userleadActor2 = document.querySelector('#leadActor2');
+    const userYearReleased = document.querySelector('#yearReleased');
+    const userMoviePoster = document.querySelector('#image');
+
+    //instantiates new class
+    const Movie = new Movie(userTitle.value, userDirector.value, userleadActor1.value, userleadActor2.value, userYearReleased.value , userMoviePoster.value);
+    console.log(Movie);
+
+    const UI = new UI();
+    console.log(UI);
     
-}
+    ui.displayMovie(movie);
+
+    ui.clearValues();
+
+    ui.removeMovie(e);
+
+    e.preventDefault();
+
+})
+
+document.querySelector('.display').addEventListener('click', function(e){
+    const ui = new UI();
+
+    ui.removeMovie(e.target);
+
+    ui.clearValues();
+
+    e.preventDefault();
+})
 
 
 
-///separate event handler for removing/deleting 
-function removeMovie(e){
-    if(e.target.parentElement.classList.contains('remove-movie')){
-        e.target.parentElement.parentElement.remove();
-    }
-}
 
-function clearValues() {
-    document.getElementById("movie-form").reset();
-}
 
-clearValues();
